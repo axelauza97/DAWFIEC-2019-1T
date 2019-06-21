933dfe42df752cf15d2c2e4b8f6d1497327c742c
patricioxavi <pjtovar@espol.edu.ec>
2019-06-21 07:38:25 -0500

    fix map

--- DAW/plantilla/oneschool/js/main.js
+++ DAW/plantilla/oneschool/js/main.js
@@ -8,15 +8,7 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
-	var mymap = L.map('mapid').setView([-2.1566588, -79.8974561,17], 15);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiYWF2ZW5kYW4iLCJhIjoiY2p3NnVzdHozMjdxeDQzcXBnYjlwMTRqcyJ9.S00xReWyD9_Eb4B1h-VgIg'
-}).addTo(mymap);
 
-	L.marker([-2.1566588, -79.8974561,17]).addTo(mymap);
 
 	var siteMenuClone = function() {
 
