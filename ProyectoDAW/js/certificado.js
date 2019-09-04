bf0e0bcfe3b8d29a54535318da7cfe8a84aa46a4
axelauza <axelauza31@gmail.com>
2019-09-04 10:57:37 -0500

    Fixes GET certificado

--- ProyectoDAW/js/certificado.js
+++ ProyectoDAW/js/certificado.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
+    var URL = "http://127.0.0.1:8000/api/";
     $.ajax({
         type: 'GET',
-        url: "http://127.0.0.1:8000/api/certificado/",
+        url: URL+"certificado/",
         crossDomain: true,
         headers: { 'Authorization': 'Token ad1f14ec4c59bae7d44504110841a59575ef32be' },
         data: { get_param: 'value' },
         dataType: "json",
 
         success: function(respuesta) {
+            $("#contenedor_Datos").empty();
             for (var j = 0; j < respuesta.length; j++) {
                 var cc = respuesta[j].cod_certificado;
-                var ne = respuesta[j].nombre_empresa;
-                var re = respuesta[j].representante;
-                var tc = respuesta[j].tipo_certificado;
-                var ca = respuesta[j].cod_Auditor;
+                var ne = respuesta[j].cod_cliente.nombre_cliente;
+                var re = respuesta[j].cod_cliente.representante;
+                var tc = "["+respuesta[j].tipo_certificado.tipo_certificado+"]"+
+                respuesta[j].tipo_certificado.descripcion;
+                var ca = respuesta[j].cod_Auditor.nombres_auditor+" "+
+                respuesta[j].cod_Auditor.apellidos_auditor;
                 var ob = respuesta[j].observaciones;
                 var ct = respuesta[j].costo;
                 $("#contenedor_datos").append(
