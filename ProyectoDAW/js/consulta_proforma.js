d9282643591ace0ef4ffac8adf542ae5a4c49c19
drios96 <dprios@espol.edu.ec>
2019-09-05 01:35:39 -0500

    search para proforma y certificado

--- ProyectoDAW/js/consulta_proforma.js
+++ ProyectoDAW/js/consulta_proforma.js
@@ -280,4 +280,23 @@ $(document).ready(function() {
 
     });
 
+    $("#myInput").on("keyup", function() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("tabla_pro");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }       
+        }
+      });
+
 })
--- ProyectoDAW/js/consultar_certificado.js
+++ ProyectoDAW/js/consultar_certificado.js
@@ -198,5 +198,24 @@ $(document).ready(function() {
     overlay.classList.remove('active');
     popup.classList.remove('active');
     });
+    $("#myInput").on("keyup", function() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("tabla_cert");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }       
+        }
+      });
+
 })
 
