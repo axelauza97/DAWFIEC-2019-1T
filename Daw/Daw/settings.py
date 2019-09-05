e112ea8426abb726bec50b02a2165bf475a80ce7
patricioxavi8 <43795184+patricioxavi8@users.noreply.github.com>
2019-09-05 08:14:49 -0500

    Update settings.py

--- Daw/Daw/settings.py
+++ Daw/Daw/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = 
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
